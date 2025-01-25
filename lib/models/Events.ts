dcf24fcfd93c0117ffa398a725cfe54ae62dec92
gt-yordanos <gt.yordanos@gmail.com>
2025-01-25 12:42:32 +0300

    created a model for events

---
+++ lib/models/Events.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, model, models } from 'mongoose';
+
+const EventSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  attendees: [{
+    type: String,
+    enum: ['Huesa Members', 'Economics Students', 'Accounting Students', 'Management Students', 'PADM Students', 'Any one'],
+    required: true,
+  }],
+});
+
+const Event = models.Event || model('Event', EventSchema);
+
+export default Event;
